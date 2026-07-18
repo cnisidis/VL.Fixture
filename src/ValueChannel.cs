6f0244e8cdea8a04ea56ac953926e8d69270c8f8
CNISIDIS <makeithappen@nisidis.com>
2026-07-18 15:48:11 +0300

    Experimental ValueChannel to correlate attributes with actual DMX channel values (FixtureDefinition->Attribute->vvvv->DMX)

---
+++ src/ValueChannel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fixture
+{
+    public abstract class AbstractValueChannel
+    {
+        /// <summary>
+        /// Value Channel unique name
+        /// </summary>
+        public string Name { get; private set; }
+        /// <summary>
+        /// Corresponding Logical Channels (Definition->ActiveMode->Channels)
+        /// </summary>
+        /// this could be just an int (to lookup) but size - resolution matters a lot, so better carry whole info
+        public Dictionary<int, GenericChannel> GenericChannels = new();
+
+
+        public object Value { get;  set; }
+
+        public AbstractValueChannel(string Name)
+        {
+            this.Name = Name;
+        }
+
+        //public abstract Spread<byte> ToBytes();
+        
+
+    }
+
+    public class ValueChannel<T> : AbstractValueChannel
+    {
+        public ValueChannel(string Name):base(Name)
+        {
+
+        }
+
+        public void SetValue(T value)
+        {
+            this.Value = value;
+        }
+
+        public T GetValue()=>(T)this.Value;
+
+    }
+}
